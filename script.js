cbcd0a2f35c4d42c1e0a107087f3490488a813e5
tomasalanis8 <tomasalanis01@gmail.com>
2023-10-29 20:09:31 -0300

    Terminar CSS formulario y agregar API que contacte el gmail de la base de datos

--- script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("butAvisarme").addEventListener("click", function() {
-    var respuesta = prompt("Escribe algo:");
-    if (respuesta !== null) {
-        alert("Escribiste: " + respuesta);
-    }
-});
